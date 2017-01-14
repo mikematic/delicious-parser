a245b8f26d57884b8b4bb7821b7a4f055d79494e
Michael Hailu Kebede <Kochiiiiii@gmail.com>
2017-01-14 13:52:39 -0500

    Couple more artifacts added and refactoring of the unit test class

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#deliciousparser
+
+Program to parse bulk HTMLs downloaded from del.icio.us server using wget tool and outputs them into one JSON formatted file. This output can be used by the chrome extension import-delicious-parser.
--- src/test/java/com/deliciousparser/ParseHTMLsTest.java
+++ src/test/java/com/deliciousparser/ParseHTMLsTest.java
@@ -4,14 +4,14 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class ImportDeliciousTest extends TestCase {
+public class ParseHTMLsTest extends TestCase {
 	
 	/**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public ImportDeliciousTest( String testName )
+    public ParseHTMLsTest( String testName )
     {
         super( testName );
     }
@@ -21,7 +21,7 @@ public class ImportDeliciousTest extends TestCase {
      */
     public static Test suite()
     {
-        return new TestSuite( ImportDeliciousTest.class );
+        return new TestSuite( ParseHTMLsTest.class );
     }
 
     /**
